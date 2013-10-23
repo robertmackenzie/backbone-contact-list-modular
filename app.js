0ba3b5e83041dcd97bd00b7d0d6ff6ff35b74095
Robert Mackenzie <robertmackenzie@gmail.com>
2013-10-23 21:40:38 +0100

    Simple list build with modules

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 var APP = APP || {};
 
-(function(){
+(function(BB){
 
   Main = {};
 
-  Main.View = Backbone.View.extend({
-    el: 'body'
+  Main.View = BB.View.extend({
+
+    el: 'body',
+
+    initialize: function() {
+      this.contacts = new APP.Contact.Collection([
+                        {
+                          name: 'Sam',
+                          age: 23,
+                          occupation: 'Hairdresser'
+                        },
+                        {
+                          name: 'Sam',
+                          age: 23,
+                          occupation: 'Hairdresser'
+                        }, 
+                        {
+                          name: 'Sam',
+                          age: 23,
+                          occupation: 'Hairdresser'
+                        } 
+                      ]);
+      this.contactsView = new APP.Contact.CollectionView({ collection: this.contacts });
+      this.render();
+    },
+
+    render: function(){
+      this.$el.html(this.contactsView.el);
+      return this;
+    }
+
   });
 
   // export module
-  APP.Main = Main;
+  APP.Main = Main.View;
 
-})();
+})(Backbone);
--- modules/contact.js
+++ modules/contact.js
@@ -1,20 +1,38 @@
 var APP = APP || {};
 
-(function(){
+(function(BB){
 
   Contact = {};
 
-  Contact.Model = Backbone.Model.extend({});
+  Contact.Model = BB.Model.extend({});
 
-  Contact.Collection = Backbone.Collection.extend({
+  Contact.Collection = BB.Collection.extend({
     model: Contact.Model
   });
 
-  Contact.View = Backbone.View.extend({});
+  Contact.View = BB.View.extend({
+    tagName: 'li',
+    template: _.template('Name: <%= name %>'),
+    render: function(){
+      this.$el.html( this.template(this.model.toJSON()) );
+      return this;
+    }
+  });
 
-  Contact.CollectionView = Backbone.View.extend({});
+  Contact.CollectionView = BB.View.extend({
+    tagName: 'ul',
+    initialize: function(){
+      this.render(); 
+    },     
+    render: function(){
+      this.collection.each(function(contact){
+        this.$el.append( new Contact.View({ model: contact }).render().el );
+      }, this);
+      return this;
+    }
+  });
 
   // export module
   APP.Contact = Contact;
 
-})();
+})(Backbone);
